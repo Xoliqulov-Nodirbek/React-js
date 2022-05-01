1b26be8e6d32cb7f3c66bd52114b89bdcefb9727
Xoliqulov-Nodirbek <xoliqulov.nodirbek2003@gmail.com>
2022-05-01 23:49:00 +0500

    Added Parrots list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Header from "./components/Header/header";
+import ParrotsBody from "./components/ParrotsFilter/body";
+import ParrotsList from "./components/ParrotsList/list";
 
 // ------> Bootstrap.min.css
 import "./assets/css/bootstrap.min.css";
@@ -7,6 +9,10 @@ function App() {
     return (
             <div class="container my-5">
                 <Header />  
+                <div class="row mt-5">
+                    <ParrotsBody />
+                    <ParrotsList / >
+                </div>
             </div>
         );
     }
---
+++ src/components/ParrotsList/list.js
@@ -0,0 +1,18 @@
+const ParrotsList = () => {
+
+    return (
+        <section className="col-6">
+        <div className="d-flex justify-content-between align-items-baseline">
+          <h2 className="h4 mb-4">Parrots list</h2>
+          <p className="count">Count: 2</p>
+        </div>
+        
+        <ul className="row list-unstyled g-3 parrots-wrapper">
+
+        </ul>
+      </section>
+    )
+
+}
+
+export default ParrotsList;
